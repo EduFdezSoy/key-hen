1799d2f7e30b2d143912fa168be65f98ebb59ebd
Guillermo Vera <vfguille@gmail.com>
2020-02-02 00:05:13 +0100

    We have social

---
+++ Key-Hen/Assets/Scripts/SocialAbout.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SocialAbout : MonoBehaviour
+{
+    public void OpenBrowserTwitterJf()
+    {
+        Application.OpenURL("https://twitter.com/JuanFCrater");
+    }
+}
