f5651b9b9ac3820d2f5adbfe9c48bcae2c802777
Guillermo Vera <vfguille@gmail.com>
2020-02-02 00:31:56 +0100

    NOW we have a pretty interactive socialAbout... Or so it seems

--- Key-Hen/Assets/Scripts/SocialAbout.cs
+++ Key-Hen/Assets/Scripts/SocialAbout.cs
@@ -8,4 +8,29 @@ public class SocialAbout : MonoBehaviour
     {
         Application.OpenURL("https://twitter.com/JuanFCrater");
     }
+    
+    public void OpenBrowserItchJf()
+    {
+        Application.OpenURL("https://juanfcrater.itch.io/");
+    }
+    
+    public void OpenBrowserTwitterEdu()
+    {
+        Application.OpenURL("https://twitter.com/edufdezsoy");
+    }
+    
+    public void OpenBrowserGitHubEdu()
+    {
+        Application.OpenURL("https://github.com/edufdezsoy");
+    }
+    
+    public void OpenBrowserGitHubGuille()
+    {
+        Application.OpenURL("https://github.com/vfguille96");
+    }
+    
+    public void OpenBrowserInstagramXim()
+    {
+        Application.OpenURL("https://www.instagram.com/xim__design/");
+    }
 }
