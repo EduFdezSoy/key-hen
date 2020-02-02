9e42bb898d84a2cc44a65e4d1423e4c468640237
Guillermo Vera <vfguille@gmail.com>
2020-02-02 15:11:47 +0100

    Exit About

---
+++ Key-Hen/Assets/Scripts/ExitAbout.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ExitAbout : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Escape))
+            SceneManager.LoadScene("Menu");
+    }
+}
